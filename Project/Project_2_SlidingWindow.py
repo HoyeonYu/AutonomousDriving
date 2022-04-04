505b158667a046b7c238d3449b43b383d065c3cc
HoyeonYu <53277342+HoyeonYu@users.noreply.github.com>
2022-04-04 11:22:24 +0900

    Update Project_2_SlidingWindow.py

--- Project/Project_2_SlidingWindow.py
+++ Project/Project_2_SlidingWindow.py
@@ -12,7 +12,6 @@ import signal
 import sys
 import os
 
-
 def signal_handler(sig, frame):
     import time
     time.sleep(3)
@@ -28,7 +27,7 @@ motor = None
 
 CAM_FPS = 30
 WIDTH, HEIGHT = 640, 480
-ROI_ROW = 250  # ROI row
+ROI_ROW = 250  # ROI row 
 ROI_HEIGHT = HEIGHT - ROI_ROW
 L_ROW = ROI_HEIGHT - 120  # Row for position detection
 LANE_WIDTH = WIDTH * 0.8
@@ -44,11 +43,11 @@ CYAN_COLOR = (255, 255, 0)
 IMG_BORDER = 300
 N_WINDOWS = 15
 WINDOW_HEIGHT = np.int(HEIGHT / N_WINDOWS)
-WINDOW_MARGIN = 60
-TARGET_X_IDX = 1
 
 CAR_PIXEL = 73
 METER_PER_PIXEL = 0.055
+
+TARGET_X_IDX = 1
 
 first_drive_flag = True
 left_found_flag, right_found_flag = [], []
@@ -64,7 +63,6 @@ imu_callback_time = None
 vel_x, vel_y, cur_vel, prev_vel = 0, 0, 0, 0
 
 
-# Moving Average Filter Class
 class MovingAverage:
     def __init__(self, n):
         self.samples = n
@@ -91,12 +89,12 @@ class MovingAverage:
 
 
 MV_AVG_SIZE = 20
+WINDOW_MARGIN = 60
 
 cur_left_x_mv = MovingAverage(MV_AVG_SIZE)
 cur_right_x_mv = MovingAverage(MV_AVG_SIZE)
 cur_left_x_mv.add_sample(WIDTH * 0.1)
 cur_right_x_mv.add_sample(WIDTH * 0.9)
-
 
 # Camera Image Topic Callback
 def img_callback(data):
@@ -104,7 +102,7 @@ def img_callback(data):
     image = bridge.imgmsg_to_cv2(data, "bgr8")
 
 
-# IMU Topic Callback for Checking Car Velocity
+# IMU Topic Callback for Checking Velocity of Car
 def imu_callback(data):
     global imu_callback_time, vel_x, vel_y, cur_vel, prev_vel, first_drive_flag
 
@@ -121,6 +119,7 @@ def imu_callback(data):
     vel_y += (data.linear_acceleration.y * dt)
 
     cur_vel = math.sqrt(math.pow(vel_x, 2) + math.pow(vel_y, 2))
+    print('cur_vel', cur_vel)
 
     # Velocity == 0 if Same Velocity
     if cur_vel == prev_vel:
@@ -145,7 +144,7 @@ def binarize_image(src_img):
 
 # Warp Image for Sliding Window
 def warp_image(img):
-    # Set Points of Source Image
+    # Set Points of Source Image 
     top_y_offset = HEIGHT * 0.65
     below_y_offset = HEIGHT * 0.8
     tl_offset = WIDTH * 0.28
@@ -166,7 +165,7 @@ def warp_image(img):
     # Get Perspective Transform Matrix
     warp_mat = cv2.getPerspectiveTransform(src_pt, dst_pt)
     warp_inverse_mat = cv2.getPerspectiveTransform(dst_pt, src_pt)
-
+    
     # Get Translated Image
     dst_img = cv2.warpPerspective(img, warp_mat, (WIDTH, HEIGHT))
     cv2.imshow('dst_img', dst_img)
@@ -174,25 +173,25 @@ def warp_image(img):
     # Border Points
     border_top_y_offset = HEIGHT * 0.65
     border_below_y_offset = HEIGHT * 0.8
-    border_tl_offset = ((tl_offset * IMG_BORDER) / (WIDTH / 2)) + tl_offset
-    border_tr_offset = (WIDTH + 2 * IMG_BORDER) - border_tl_offset
-    border_bl_offset = ((bl_offset * IMG_BORDER) / (WIDTH / 2)) + bl_offset
-    border_br_offset = (WIDTH + 2 * IMG_BORDER) - border_bl_offset
+    border_tl_offset = ((tl_offset * IMG_BORDER) / (WIDTH/2)) + tl_offset
+    border_tr_offset = (WIDTH+2*IMG_BORDER) - border_tl_offset
+    border_bl_offset = ((bl_offset * IMG_BORDER) / (WIDTH/2)) + bl_offset
+    border_br_offset = (WIDTH+2*IMG_BORDER) - border_bl_offset
 
     border_src_tl = [border_tl_offset, border_top_y_offset]
     border_src_tr = [border_tr_offset, border_top_y_offset]
     border_src_bl = [border_bl_offset, border_below_y_offset]
     border_src_br = [border_br_offset, border_below_y_offset]
-
+    
     border_src_pt = np.float32([border_src_tl, border_src_tr, border_src_bl, border_src_br])
-    border_dst_pt = np.float32([[0, 0], [WIDTH + 2 * IMG_BORDER, 0], [0, HEIGHT], [WIDTH + 2 * IMG_BORDER, HEIGHT]])
+    border_dst_pt = np.float32([[0, 0], [WIDTH+2*IMG_BORDER, 0], [0, HEIGHT], [WIDTH+2*IMG_BORDER, HEIGHT]])
 
     # Get Perspective Transform Matrix
     border_warp_mat = cv2.getPerspectiveTransform(border_src_pt, border_dst_pt)
     border_warp_inverse_mat = cv2.getPerspectiveTransform(border_dst_pt, border_src_pt)
-
+    
     # Get Translated Image
-    border_dst_img = cv2.warpPerspective(img, border_warp_mat, (WIDTH + 2 * IMG_BORDER, HEIGHT))
+    border_dst_img = cv2.warpPerspective(img, border_warp_mat, (WIDTH+2*IMG_BORDER, HEIGHT))
 
     return dst_img, border_warp_inverse_mat, src_pt
 
@@ -221,7 +220,7 @@ def check_same_lane():
     left_dx1 = abs(x_left_list[y1_point] - prev_x_left_list[y1_point])
     left_dx2 = abs(x_left_list[y2_point] - prev_x_left_list[y2_point])
     left_dx3 = abs(x_left_list[y3_point] - prev_x_left_list[y3_point])
-
+    
     right_dx1 = abs(x_right_list[y1_point] - prev_x_right_list[y1_point])
     right_dx2 = abs(x_right_list[y2_point] - prev_x_right_list[y2_point])
     right_dx3 = abs(x_right_list[y3_point] - prev_x_right_list[y3_point])
@@ -234,7 +233,7 @@ def check_same_lane():
         print('Not All Same')
         x_left_list = prev_x_left_list[:]
         x_right_list = prev_x_right_list[:]
-
+    
     else:
         if left_diff > MAX_AVG_GAP:
             print('Left Not Same')
@@ -282,8 +281,7 @@ def sliding_window(bin_img):
     y_left_list, y_right_list = [], []
 
     sliding_img = np.dstack((bin_img, bin_img, bin_img)) * 255
-    sliding_img = cv2.copyMakeBorder(sliding_img, 0, 0, IMG_BORDER, IMG_BORDER, cv2.BORDER_CONSTANT,
-                                     cv2.BORDER_CONSTANT)
+    sliding_img = cv2.copyMakeBorder(sliding_img, 0, 0, IMG_BORDER, IMG_BORDER, cv2.BORDER_CONSTANT, cv2.BORDER_CONSTANT)
 
     prev_left_x = cur_left_x
     prev_left_diff = 0
@@ -312,9 +310,9 @@ def sliding_window(bin_img):
         win_xrh = cur_right_x + WINDOW_MARGIN
 
         left_found_upper_part = \
-            ((nz[0] >= win_yl) & (nz[0] < win_yl_part) & (nz[1] >= win_xll) & (nz[1] < win_xlh)).nonzero()[0]
+        ((nz[0] >= win_yl) & (nz[0] < win_yl_part) & (nz[1] >= win_xll) & (nz[1] < win_xlh)).nonzero()[0]
         right_found_upper_part = \
-            ((nz[0] >= win_yl) & (nz[0] < win_yl_part) & (nz[1] >= win_xrl) & (nz[1] < win_xrh)).nonzero()[0]
+        ((nz[0] >= win_yl) & (nz[0] < win_yl_part) & (nz[1] >= win_xrl) & (nz[1] < win_xrh)).nonzero()[0]
 
         # Both Left, Right Window Not Found
         if (len(left_found_upper_part) < MIN_PIX) and (len(right_found_upper_part) < MIN_PIX):
@@ -322,7 +320,7 @@ def sliding_window(bin_img):
             cur_right_x = prev_right_x + prev_right_diff
             left_found_flag.append(False)
             right_found_flag.append(False)
-
+        
         else:
             # Left Window Found
             if len(left_found_upper_part) > MIN_PIX:
@@ -375,10 +373,11 @@ def sliding_window(bin_img):
         prev_right_found_flag = True
 
     if not first_drive_flag:
-        check_same_lane()
+       check_same_lane()
     else:
         prev_left_found_flag = False
         prev_right_found_flag = False
+
 
     first_drive_flag = False
 
@@ -392,7 +391,7 @@ def sliding_window(bin_img):
 
 
 # Publish Motor Topic
-def motor_drive(Angle, Speed):
+def motor_drive(Angle, Speed): 
     global motor
 
     motor_msg = xycar_motor()
@@ -414,7 +413,7 @@ def get_drive_angle(dist):
 
     diff_x = ((x_left_list[idx_y] + x_right_list[idx_y]) / 2) - (WIDTH / 2)
     print('diff_x', diff_x)
-
+        
     control_grad = diff_x / float(target_y + CAR_PIXEL)
     control_angle = math.degrees(math.atan(control_grad))
     print('control_angle', control_angle)
@@ -428,8 +427,8 @@ def fill_warped_lane(image, warp_img, border_warp_inverse_mat, left_fit, right_f
     plot_y = np.linspace(0, HEIGHT - 1, HEIGHT)
     color_warp = np.zeros_like(warp_img).astype(np.uint8)
 
-    left_fit_x = left_fit[0] * (plot_y ** 2) + left_fit[1] * plot_y + left_fit[2] + IMG_BORDER
-    right_fit_x = right_fit[0] * (plot_y ** 2) + right_fit[1] * plot_y + right_fit[2] + IMG_BORDER
+    left_fit_x = left_fit[0] * (plot_y**2) + left_fit[1]*plot_y + left_fit[2] + IMG_BORDER
+    right_fit_x = right_fit[0] * (plot_y**2) + right_fit[1]*plot_y + right_fit[2] + IMG_BORDER
 
     left_points = np.array([np.transpose(np.vstack([left_fit_x, plot_y]))])
     right_points = np.array([np.flipud(np.transpose(np.vstack([right_fit_x, plot_y])))])
@@ -438,8 +437,8 @@ def fill_warped_lane(image, warp_img, border_warp_inverse_mat, left_fit, right_f
     color_warp = cv2.fillPoly(color_warp, np.int_([points]), GREEN_COLOR)
     cv2.imshow('color_warp', color_warp)
 
-    new_warp = cv2.warpPerspective(color_warp, border_warp_inverse_mat, (WIDTH + 2 * IMG_BORDER, HEIGHT))
-    new_warp = new_warp[:, IMG_BORDER:IMG_BORDER + WIDTH]
+    new_warp = cv2.warpPerspective(color_warp, border_warp_inverse_mat, (WIDTH+2*IMG_BORDER, HEIGHT))
+    new_warp = new_warp[:, IMG_BORDER:IMG_BORDER+WIDTH]
     cv2.imshow('new_warp', new_warp)
     new_lane_img = cv2.addWeighted(image, 1, new_warp, 0.3, 0)
 
@@ -479,27 +478,26 @@ def draw_img(src_img, src_pt, drive_angle, target_y, bin_lane_img, sliding_img):
         left_window_color = found_color if left_found_flag[window_idx] else predict_color
         right_window_color = found_color if right_found_flag[window_idx] else predict_color
 
-        cv2.rectangle(sliding_img, (int(IMG_BORDER + win_xll), int(win_yl)),
-                      (int(IMG_BORDER + win_xlh), int(win_yh)), left_window_color, 2)
-        cv2.rectangle(sliding_img, (int(IMG_BORDER + win_xrl), int(win_yl)),
-                      (int(IMG_BORDER + win_xrh), int(win_yh)), right_window_color, 2)
+        cv2.rectangle(sliding_img, (int(IMG_BORDER + win_xll), int(win_yl)), 
+                        (int(IMG_BORDER + win_xlh), int(win_yh)), left_window_color, 2)
+        cv2.rectangle(sliding_img, (int(IMG_BORDER + win_xrl), int(win_yl)), 
+                        (int(IMG_BORDER + win_xrh), int(win_yh)), right_window_color, 2)
 
         if window_idx > 0:
             prev_center_x = (x_left_list[window_idx - 1] + x_right_list[window_idx - 1]) / 2
             cur_center_x = (x_left_list[window_idx] + x_right_list[window_idx]) / 2
 
-            cv2.line(sliding_img, (int(IMG_BORDER + prev_center_x), int(win_yh + WINDOW_HEIGHT)),
-                     (int(IMG_BORDER + cur_center_x), int(win_yl + WINDOW_HEIGHT)), MAGENTA_COLOR, 2)
+            cv2.line(sliding_img, (int(IMG_BORDER+prev_center_x), int(win_yh+WINDOW_HEIGHT)),
+                    (int(IMG_BORDER+cur_center_x), int(win_yl+WINDOW_HEIGHT)), MAGENTA_COLOR, 2)
 
     lane_img = cv2.cvtColor(bin_lane_img, cv2.COLOR_GRAY2BGR)
     lane_img = cv2.copyMakeBorder(lane_img, 0, 0, IMG_BORDER, IMG_BORDER, cv2.BORDER_CONSTANT, cv2.BORDER_CONSTANT)
 
     sliding_img = cv2.bitwise_or(sliding_img, lane_img)
 
-    cv2.line(sliding_img, (0, int(HEIGHT - target_y)), (int(IMG_BORDER * 2 + WIDTH), int(HEIGHT - target_y)),
-             GRAY_COLOR, 2)
-    cv2.putText(sliding_img, 'angle: ' + str(drive_angle)[:5], (int(IMG_BORDER * 2 + WIDTH * 0.7),
-                                                                int(HEIGHT * 0.1)), 1, 1, GRAY_COLOR, 1)
+    cv2.line(sliding_img, (0, int(HEIGHT - target_y)), (int(IMG_BORDER*2 + WIDTH), int(HEIGHT - target_y)), GRAY_COLOR, 2)
+    cv2.putText(sliding_img, 'angle: ' + str(drive_angle)[:5], (int(IMG_BORDER*2 + WIDTH * 0.7), 
+                int(HEIGHT * 0.1)), 1, 1, GRAY_COLOR, 1)
     cv2.imshow('new_sliding_window', sliding_img)
     cv2.waitKey(1)
 
@@ -530,37 +528,36 @@ prev_minpix = 5
 
 prev_lane_bin_th = 145
 
-
 def prev_code_warp_process_image(src_img):
     global prev_nwindows, prev_WINDOW_MARGIN, prev_minpix, prev_lane_bin_th
 
-    blur = cv2.GaussianBlur(src_img, (5, 5), 0)
+    blur = cv2.GaussianBlur(src_img,(5, 5), 0)
     _, L, _ = cv2.split(cv2.cvtColor(blur, cv2.COLOR_BGR2HLS))
     _, bin_lane_img = cv2.threshold(L, prev_lane_bin_th, 255, cv2.THRESH_BINARY)
 
-    histogram = np.sum(bin_lane_img[bin_lane_img.shape[0] // 2:, :], axis=0)
-    midpoint = np.int(histogram.shape[0] / 2)
+    histogram = np.sum(bin_lane_img[bin_lane_img.shape[0]//2:,:], axis=0)      
+    midpoint = np.int(histogram.shape[0]/2)
     leftx_current = np.argmax(histogram[:midpoint])
     rightx_current = np.argmax(histogram[midpoint:]) + midpoint
 
-    WINDOW_HEIGHT = np.int(bin_lane_img.shape[0] / prev_nwindows)
+    WINDOW_HEIGHT = np.int(bin_lane_img.shape[0]/prev_nwindows)
     nz = bin_lane_img.nonzero()
 
     left_lane_inds = []
     right_lane_inds = []
-
+    
     lx, ly, rx, ry = [], [], [], []
 
-    out_img = np.dstack((bin_lane_img, bin_lane_img, bin_lane_img)) * 255
+    out_img = np.dstack((bin_lane_img, bin_lane_img, bin_lane_img))*255
     out_img = cv2.copyMakeBorder(out_img, 0, 0, IMG_BORDER, IMG_BORDER, cv2.BORDER_CONSTANT,
-                                 cv2.BORDER_CONSTANT)
+                                     cv2.BORDER_CONSTANT)
 
     prev_left_x = leftx_current
     prev_right_x = rightx_current
 
     for window in range(prev_nwindows):
-        win_yl = bin_lane_img.shape[0] - (window + 1) * WINDOW_HEIGHT
-        win_yh = bin_lane_img.shape[0] - window * WINDOW_HEIGHT
+        win_yl = bin_lane_img.shape[0] - (window+1)*WINDOW_HEIGHT
+        win_yh = bin_lane_img.shape[0] - window*WINDOW_HEIGHT
 
         win_xll = leftx_current - prev_WINDOW_MARGIN
         win_xlh = leftx_current + prev_WINDOW_MARGIN
@@ -568,26 +565,26 @@ def prev_code_warp_process_image(src_img):
         win_xrh = rightx_current + prev_WINDOW_MARGIN
 
         cv2.rectangle(out_img, (int(IMG_BORDER + win_xll), int(win_yl)), (int(IMG_BORDER + win_xlh), int(win_yh)),
-                      (0, 255, 0), 2)
+                      (0,255,0), 2) 
         cv2.rectangle(out_img, (int(IMG_BORDER + win_xrl), int(win_yl)), (int(IMG_BORDER + win_xrh), int(win_yh)),
-                      (0, 255, 0), 2)
+                      (0,255,0), 2) 
 
-        good_left_inds = ((nz[0] >= win_yl) & (nz[0] < win_yh) & (nz[1] >= win_xll) & (nz[1] < win_xlh)).nonzero()[0]
-        good_right_inds = ((nz[0] >= win_yl) & (nz[0] < win_yh) & (nz[1] >= win_xrl) & (nz[1] < win_xrh)).nonzero()[0]
+        good_left_inds = ((nz[0] >= win_yl)&(nz[0] < win_yh)&(nz[1] >= win_xll)&(nz[1] < win_xlh)).nonzero()[0]
+        good_right_inds = ((nz[0] >= win_yl)&(nz[0] < win_yh)&(nz[1] >= win_xrl)&(nz[1] < win_xrh)).nonzero()[0]
 
         left_lane_inds.append(good_left_inds)
         right_lane_inds.append(good_right_inds)
 
         if len(good_left_inds) > prev_minpix:
             leftx_current = np.int(np.mean(nz[1][good_left_inds]))
-        if len(good_right_inds) > prev_minpix:
+        if len(good_right_inds) > prev_minpix:        
             rightx_current = np.int(np.mean(nz[1][good_right_inds]))
 
         lx.append(leftx_current)
-        ly.append((win_yl + win_yh) / 2)
+        ly.append((win_yl + win_yh)/2)
 
         rx.append(rightx_current)
-        ry.append((win_yl + win_yh) / 2)
+        ry.append((win_yl + win_yh)/2)
 
         prev_center_x = (prev_left_x + prev_right_x) / 2
         cur_center_x = (leftx_current + rightx_current) / 2
@@ -601,11 +598,11 @@ def prev_code_warp_process_image(src_img):
     left_lane_inds = np.concatenate(left_lane_inds)
     right_lane_inds = np.concatenate(right_lane_inds)
 
-    # left_fit = np.polyfit(nz[0][left_lane_inds], nz[1][left_lane_inds], 2)
-    # right_fit = np.polyfit(nz[0][right_lane_inds] , nz[1][right_lane_inds], 2)
-
-    lfit = np.polyfit(np.array(ly), np.array(lx), 2)
-    rfit = np.polyfit(np.array(ry), np.array(rx), 2)
+    #left_fit = np.polyfit(nz[0][left_lane_inds], nz[1][left_lane_inds], 2)
+    #right_fit = np.polyfit(nz[0][right_lane_inds] , nz[1][right_lane_inds], 2)
+    
+    lfit = np.polyfit(np.array(ly),np.array(lx),2)
+    rfit = np.polyfit(np.array(ry),np.array(rx),2)
 
     lane_img = cv2.cvtColor(bin_lane_img, cv2.COLOR_GRAY2BGR)
     lane_img = cv2.copyMakeBorder(lane_img, 0, 0, IMG_BORDER, IMG_BORDER, cv2.BORDER_CONSTANT, cv2.BORDER_CONSTANT)
@@ -616,26 +613,26 @@ def prev_code_warp_process_image(src_img):
     # out_img[nz[0][right_lane_inds], IMG_BORDER + nz[1][right_lane_inds]] = RED_COLOR
 
     cv2.imshow("prev_sliding_img", out_img)
-
+    
     return lfit, rfit, out_img, bin_lane_img
-
 
 def prev_code_fill_warped_lane(image, warp_img, Minv, left_fit, right_fit):
     yMax = warp_img.shape[0]
     ploty = np.linspace(0, yMax - 1, yMax)
     color_warp = np.zeros_like(warp_img).astype(np.uint8)
-
-    left_fitx = left_fit[0] * (ploty ** 2) + left_fit[1] * ploty + left_fit[2]
-    right_fitx = right_fit[0] * (ploty ** 2) + right_fit[1] * ploty + right_fit[2]
-
+    
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+    
     left_points = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
-    right_points = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+    right_points = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))]) 
     pts = np.hstack((left_points, right_points))
-
+    
     color_warp = cv2.fillPoly(color_warp, np.int_([pts]), (0, 255, 0))
     newwarp = cv2.warpPerspective(color_warp, Minv, (WIDTH, HEIGHT))
 
     return cv2.addWeighted(image, 1, newwarp, 0.3, 0)
+
 
 
 ##################################################################################################################
@@ -672,14 +669,14 @@ def start():
         # #################    Previous Code    ###################
         # #########################################################
 
-        # prev_code_src_img = image.copy()
-        # prev_code_warp_img = warp_img.copy()
-        # prev_code_warp_inverse_mat = border_warp_inverse_mat.copy()
-        # prev_code_move_dist = move_dist
-        # prev_code_src_pt = src_pt
-        # prev_code_left_fit, prev_code_right_fit, prev_code_sliding_img, prev_code_bin_lane_img = prev_code_warp_process_image(prev_code_warp_img)
-        # prev_code_lane_img = prev_code_fill_warped_lane(prev_code_src_img, warp_img, prev_code_warp_inverse_mat, prev_code_left_fit, prev_code_right_fit)
-        # prev_code_drive_angle, prev_code_target_y = get_drive_angle(prev_code_move_dist)
+        prev_code_src_img = image.copy()
+        prev_code_warp_img = warp_img.copy()
+        prev_code_warp_inverse_mat = border_warp_inverse_mat.copy()
+        prev_code_move_dist = move_dist
+        prev_code_src_pt = src_pt
+        prev_code_left_fit, prev_code_right_fit, prev_code_sliding_img, prev_code_bin_lane_img = prev_code_warp_process_image(prev_code_warp_img)
+        prev_code_lane_img = prev_code_fill_warped_lane(prev_code_src_img, warp_img, prev_code_warp_inverse_mat, prev_code_left_fit, prev_code_right_fit)
+        prev_code_drive_angle, prev_code_target_y = get_drive_angle(prev_code_move_dist)
 
         # #########################################################
 
